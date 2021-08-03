37ffd2108c53aa18736e4c1161e54ed90092e5fb
wisara88 <srandall845@gmail.com>
2021-08-02 20:48:07 -0700

    data added in JSON

---
+++ json_date.js
@@ -0,0 +1,52 @@
+
+const data = [
+  {
+    categories: ["Vans"],
+    title: "The A Team",
+    image: "https://imgur.com/gallery/OSzSt",
+    pros: ["stealth", "rocket launchers", "Mr. T"],
+    cons: ["Mr. T's accent", "expensive"],
+    blurb: "Maybe you can hire the A-Team",
+  },
+  {
+    categories: ["Vans"],
+    title: "Mystery Machine",
+    image: "https://imgur.com/gallery/pc0fqrb",
+    pros: ["Scooby", "snacks", "mystery solvers"],
+    cons: ["grass", "a little too cheap", "poor stealthing abilities"],
+    blurb: "It's mean, it's green, it's the mystery machine!... and it's a Ford Transit",
+  },
+  {
+    categories: ["Vans"],
+    title: "The Party Wagon",
+    image: "https://static.wikia.nocookie.net/hotwheels/images/b/b5/2020NM26_%28Large%29.JPG/revision/latest/scale-to-width-down/1000?cb=20200329024016",
+    pros: ["Nunchuks", "Swords", "Pizza"],
+    cons: ["it's yellow", "fuel efficency"],
+    blurb: "Turtles in a half shell... TURTLE POWER!",
+  },
+  {
+    categories: ["Weapons"],
+    title: "Rocket Launcher",
+    image: "https://imgur.com/gallery/B1SpX2Z",
+    pros: ["BAM", "no accuracy required", "Like, no accuracy at all"],
+    cons: ["expensive", "loud", "slow reload"],
+    blurb: "one stop problem solver"
+  },
+  {
+    categories: ["Weapons"],
+    title: "Flame Thrower",
+    image: "https://imgur.com/gallery/GJLKafT",
+    pros: ["All of them", "fire", "throwing fire", "cool factor"],
+    cons: ["None", "Range", "fuel economy", "diminishes visibilility"],
+    blurb: "NIAHAHAHAHHA - Skeletor"
+  },
+  {
+    categories: ["Weapons"],
+    title: "Paintball Gun",
+    image: "https://imgur.com/gallery/WwooBND",
+    pros: ["non-lethal", "price", "colour variety", "silent"],
+    cons: ["non-lethal", "splatter",],
+    blurb: "I love the smell of fresh paint in the morning"
+  },
+]
+
--- src/PaneManagerWidget.js
+++ src/PaneManagerWidget.js
@@ -4,13 +4,40 @@ const SideBar = () => {
   return (
     <aside className="sidebar">
 
+
     </aside>
   )
 }
+
+const OneThirdTwoThirds = (props) => {
+  return (
+    <main className='one-third-two-thirds'>
+      { props.children}
+    </main>
+  )
+}
+
+const VansCategory = () => {
+  return (
+    <OneThirdTwoThirds>
+      <aside></aside>
+      <section></section>
+    </OneThirdTwoThirds>
+  )
+}
+
+const WeaponsCategory = () => (
+  <OneThirdTwoThirds>
+    <aside></aside>
+    <section></section>
+  </OneThirdTwoThirds>
+);
+
 const ContentArea = () => {
   return (
     <section className="content-area">
-
+      { true && <VansCategory></VansCategory>}
+      { false && <WeaponsCategory></WeaponsCategory>}
     </section>
   )
 }
@@ -29,6 +56,7 @@ const PaneManagerWidget = () => {
       <ContentArea />
       <BottomBar />
     </main>
+
 
   )
 }
