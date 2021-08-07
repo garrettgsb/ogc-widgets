76580e53414f61de8da075617a8b2ef3c7e9ffbe
wisara88 <srandall845@gmail.com>
2021-08-06 20:53:56 -0700

    selector refractor

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -1,25 +1,22 @@
 function completeCategories(selections, appCategories, appData) {
-  const categoryList = [];
-  // add a check mark to every category in selections
-  const itemTitles = [];
-  //for each key in selections return a category
-  // make into a filter
-  for (const selection in selections) {
-    if (selections[selection] === true) {
-      itemTitles.push(selection);
-    }
-  }
+  // const itemTitles = [];
+  // still an object
+  // itemTitles.selections.filter((selection) => selection);
+  // const categoryList = [];
+  const itemTitles = Object.entries(selections).filter(([key, val]) => val).map(([key, val]) => key);
 
   // make into a reduce
-  const newItem = itemTitles.forEach((title) => { // title = 'Mystery Machine'
-    const matchingItem = appData.find((item) => { // item = { title: 'Mystery Machine', ... }
-      return item.title === title;
-    })
-    const found = matchingItem.categories;
-    categoryList.concat(found);
-  })
+  // itemTitles.forEach((title) => { // title = 'Mystery Machine'
+  //   const matchingItem = appData.find((item) => { // item = { title: 'Mystery Machine', ... }
+  //     return item.title === title;
+  //   })
+  //   ;
+  //   categoryList.concat(matchingItem.categories);
+  // })
 
-  return categoryList;
+  return itemTitles.reduce((acc, title) => [...acc, ...(appData.find((item) => item.title === title).categories)], [])
+
+  // return categoryList;
 }
 
 function garrettsCompleteCategories(selections, appCategories, appData) {
@@ -28,35 +25,3 @@ function garrettsCompleteCategories(selections, appCategories, appData) {
 }
 
 export default completeCategories;
-
-
-// const selectors = {
-//   'Mystery Machine': true,
-//   'Party Wagon': false,
-//   'Flamethrower': true,
-// };
-
-// const appCategories = {
-//   vans: 'Vans',
-//   weapons: 'Weapons',
-//   sensors: 'Sensors',
-// }
-
-// const appData = [
-
-// ];
-
-// item = appData.find(item => item.title === 'Mystery Machine');
-// item.categories;
-
-
-// // should return...?
-// ['Vans', 'Sensors', 'Vans', 'Vans', 'Sensors']; // No weapons because no weapons selected?
-// {
-//   Vans: true,
-//   Weapons: false,
-//   Sensors: true
-// }; // Maybe that instead?
-
-// {categories.map(category => <li>{categoriesWithSelections.includes(category) && "☑️"}</li>)}
-// {categories.map(category => <li>{categoriesWithSelections[category] && "☑️"}</li>)}
