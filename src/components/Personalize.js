ef2111aef7d5c5013f5fe0e45065ecced7d3929d
wisara88 <srandall845@gmail.com>
2021-09-08 14:53:28 -0700

    slider selector working

--- src/components/Personalize.js
+++ src/components/Personalize.js
@@ -1,15 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserInputContext } from "../contexts";
 
 const Personalize = () => {
   const { title, updateTitle, budget, updateBudget, isOffgrid, updateIsOffgrid } = useContext(UserInputContext);
+  // let tripLength = "A couple of nights";
+  const [tripLength, setTripLength] = useState("A couple of nights");
+
+  function sliderTripValue(value) {
+    switch (value) {
+      case "1":
+        setTripLength("A couple of nights");
+        break
+      case "2":
+        setTripLength("Less than 1 week");
+        break
+      case "3":
+        setTripLength("1 to 2 weeks");
+        break;
+      case "4":
+        setTripLength("3 to 6 weeks");
+        break;
+      case "5":
+        setTripLength("Months long expeditions");
+        break;
+      default:
+        setTripLength("default");
+        break;
+
+    }
+  }
+
   return (
     <div className="personalize cardlike">
       <h1>Hello,</h1>
       <h1>{title}</h1>
       <h4>Project Name</h4>
       <input type="text" style={{ zIndex: 1 }} onChange={(e) => updateTitle(e.target.value)} />
-      <h4>Set a Budget ${budget}</h4>
+      <h4>Current Budget ${budget}</h4>
       <input type="text" onChange={(e) => updateBudget(e.target.value)} />
       <h4>Primarily Offgrid?</h4>
       <label>
@@ -20,7 +47,12 @@ const Personalize = () => {
         <input type="radio" checked={isOffgrid === "no"} value="no" onChange={(e) => updateIsOffgrid(e.target.value)} />
         No
       </label>
-      <p>terrain, typical trip length, full build?, </p>
+      <h4>Typical Trip Length?</h4>
+      <div>
+        <input type="range" min="1" max="5" onChange={(e) => sliderTripValue(e.target.value)} className="trip-slider" />
+        <p>{tripLength}</p>
+      </div>
+      <p>terrain, full build?, </p>
     </div>
   )
 }
